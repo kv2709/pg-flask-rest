e130a798d2493bd23bbe505b7d88c4465267e044
kv2709 <kv2709@gmail.com>
2019-07-10 23:11:09 +0700

    get author on id

---
+++ api/db.py
@@ -0,0 +1,56 @@
+from psycopg2 import connect
+
+
+def tp_to_dict(fetch_cur_in, cursor_in):
+    """ Преобразуем полученный из базы кортежей fetch_cur_in, взяв
+        дескриптор куросра базы cursor_in в словарь, ключами которого
+        являются имена полей, а значениями - значания полей базы
+    """
+    descr = cursor_in.description
+    rec = fetch_cur_in
+    d = {}
+    enu = enumerate(descr)
+    for idx, colum in enu:
+        d[colum[0]] = rec[idx]
+    return d
+
+
+def list_tp_to_list_dict(fetch_cur_in, cursor_in, key="lst"):
+    """ Преобразуем полученный из базы список кортежей или
+        кортеж fetch_cur_in, взяв дескриптор куросра базы cursor_in
+        в список словарей, ключами которого являются имена полей,
+        а значениями - значания полей базы. Работает как для fetchall
+        так и для fetchone
+    """
+    descr = cursor_in.description
+    dict_lst = []
+    cur_lst_in = []
+    if type(fetch_cur_in) == tuple:
+        cur_lst_in.append(fetch_cur_in)
+    else:
+        cur_lst_in = fetch_cur_in
+    for rec in cur_lst_in:
+        d = {}
+        enu = enumerate(descr)
+        for idxt, colum in enu:
+            d[colum[0]] = rec[idxt]
+        dict_lst.append(d)
+    if key != "lst":
+        dict_lst = tuple(dict_lst)
+
+    return dict_lst
+
+
+def get_conn_db():
+    db_name = 'd2rqo5613re182'
+    user_name = 'ntrfylfbzpwopk'
+    host_name = 'ec2-174-129-240-67.compute-1.amazonaws.com'
+    passwd = '8b2413c9d453644338573e79a45949c75db862fca0c10c4190e3d50f82fef780'
+    port_num = '5432'
+    conn = connect(dbname=db_name, user=user_name, host=host_name, password=passwd, port=port_num)
+
+    # conn = connect(dbname='test_bd', user='usr_bd', host='127.0.0.1', password='usr_pwd', port='5432')
+
+    return conn
+
+
---
+++ api/rest_app.py
@@ -0,0 +1,174 @@
+import json
+import datetime
+from flask import Flask, flash, g, redirect, render_template, request, url_for
+from werkzeug.exceptions import abort
+from flask_restful import Resource, Api
+
+from api.db import list_tp_to_list_dict, tp_to_dict, get_conn_db
+from api.utils import json_response, JSON_MIME_TYPE
+
+# Надо АПИ и обЪявление и добавдение ресурса или не надо, пока не понятно!!!!
+app = Flask(__name__)
+
+
+def convert_dt(o):
+    if isinstance(o, datetime.datetime):
+        return o.__str__()
+
+
+@app.route("/api/posts")
+def posts_list():
+    '''
+        Возвращает все записи базы на запром к УРЛу "/"
+        в json.damp изменена выдача datetime на строчное представлние
+        заменой default метода на свой в виде вызова строчного
+        представлние объекта! Гениальное решение! Надеюсь
+        :return: json со всеми записями
+    '''
+    conn = get_conn_db()
+    cur = conn.cursor()
+
+    cur.execute('''
+            SELECT post.id, title, body, created, author_id, username
+            FROM post JOIN author ON post.author_id = author.id
+            ORDER BY created DESC;
+            ''')
+
+    post_cur = cur.fetchall()
+    tp_bd = list_tp_to_list_dict(post_cur, cur)
+    cur.close()
+    conn.commit()
+    conn.close()
+    return json_response(json.dumps(tp_bd, default=convert_dt))
+
+
+@app.route("/api/<int:user_id>/author")
+def get_author_on_id(user_id):
+    '''
+    По запросу GET с user_id автора найти его в базе и вернуть словарь с данными
+    :param user_id:
+    :return:
+    '''
+    conn = get_conn_db()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM author WHERE id = %s", (user_id,))
+    auth_cur = cur.fetchone()
+    author_dic = tp_to_dict(auth_cur, cur)
+    cur.close()
+    conn.close()
+    return json_response(json.dumps(author_dic))
+
+
+# def get_post(id, check_author=True):
+#     """Get a post and its author by id.
+#
+#     Checks that the id exists and optionally that the current user is
+#     the author.
+#
+#     :param id: id of post to get
+#     :param check_author: require the current user to be the author
+#     :return: the post with author information
+#     :raise 404: if a post with the given id doesn't exist
+#     :raise 403: if the current user isn't the author
+#     """
+#     conn = get_conn_db()
+#     cur = conn.cursor()
+#     cur.execute(
+#             "SELECT post.id, title, body, created, author_id, username"
+#             " FROM post  JOIN author ON post.author_id = author.id"
+#             " WHERE post.id = %s",
+#             (id,),
+#         )
+#     cur_post = cur.fetchone()
+#     post = tp_to_dict(cur_post, cur)
+#     cur.close()
+#     conn.commit()
+#     conn.close()
+#
+#     if post is None:
+#         abort(404, "Post id {0} doesn't exist.".format(id))
+#
+#     if check_author and post["author_id"] != g.user["id"]:
+#         abort(403)
+#
+#     return post
+#
+#
+# @bp.route("/create", methods=("GET", "POST"))
+# @login_required
+# def create():
+#     """Create a new post for the current user."""
+#     if request.method == "POST":
+#         title = request.form["title"]
+#         body = request.form["body"]
+#         error = None
+#
+#         if not title:
+#             error = "Title is required."
+#
+#         if error is not None:
+#             flash(error)
+#         else:
+#             conn = get_conn_db()
+#             cur = conn.cursor()
+#             cur.execute(
+#                 "INSERT INTO post (title, body, author_id)" " VALUES (%s, %s, %s)",
+#                 (title, body, g.user["id"]),
+#             )
+#             cur.close()
+#             conn.commit()
+#             conn.close()
+#
+#             return redirect(url_for("blog.index"))
+#
+#     return render_template("blog/create.html")
+#
+#
+# @bp.route("/<int:id>/update", methods=("GET", "POST"))
+# @login_required
+# def update(id):
+#     """Update a post if the current user is the author."""
+#     post = get_post(id)
+#
+#     if request.method == "POST":
+#         title = request.form["title"]
+#         body = request.form["body"]
+#         error = None
+#
+#         if not title:
+#             error = "Title is required."
+#
+#         if error is not None:
+#             flash(error)
+#         else:
+#             conn = get_conn_db()
+#             cur = conn.cursor()
+#             cur.execute(
+#                 "UPDATE post SET title = %s, body = %s WHERE id = %s", (title, body, id)
+#             )
+#             cur.close()
+#             conn.commit()
+#             conn.close()
+#
+#             return redirect(url_for("blog.index"))
+#
+#     return render_template("blog/update.html", post=post)
+#
+#
+# @bp.route("/<int:id>/delete", methods=("POST",))
+# @login_required
+# def delete(id):
+#     """Delete a post.
+#
+#     Ensures that the post exists and that the logged in user is the
+#     author of the post.
+#     """
+#     conn = get_conn_db()
+#     cur = conn.cursor()
+#     cur.execute(
+#         "DELETE FROM post WHERE id = %s", (id,)
+#     )
+#     cur.close()
+#     conn.commit()
+#     conn.close()
+#     return redirect(url_for("blog.index"))
