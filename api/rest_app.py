cdb0e1a2b89b694862524e1c40805a82b975fef5
kv2709 <kv2709@gmail.com>
2019-07-21 11:12:54 +0700

    All funk works. Ready for deploy

--- api/rest_app.py
+++ api/rest_app.py
@@ -2,7 +2,7 @@ import json
 import datetime
 from flask import Flask, request
 from api.db import list_tp_to_list_dict, tp_to_dict, get_conn_db
-from api.utils import json_response, JSON_MIME_TYPE
+from api.utils import json_response
 
 # Надо АПИ и обЪявление и добавдение ресурса или не надо, пока не понятно!!!!
 app = Flask(__name__)
@@ -119,7 +119,7 @@ def update_post(post_id):
 
 
 @app.route("/api/posts/<int:post_id>", methods=['DELETE'])
-def udelete_post(post_id):
+def delete_post(post_id):
     """
     Удаляет в БД измененный  пост с номером post_id
     :param post_id:
@@ -133,7 +133,7 @@ def udelete_post(post_id):
     cur.close()
     conn.commit()
     conn.close()
-    #
+
     return json_response(json.dumps({"code_error": "Deleted_post"}))
 
 
@@ -195,120 +195,3 @@ def create_author():
 
     return json_response(json.dumps({"code_error": "Created_new_author"}))
 
-
-
-
-
-# def get_post(id, check_author=True):
-#     """Get a post and its author by id.
-#
-#     Checks that the id exists and optionally that the current user is
-#     the author.
-#
-#     :param id: id of post to get
-#     :param check_author: require the current user to be the author
-#     :return: the post with author information
-#     :raise 404: if a post with the given id doesn't exist
-#     :raise 403: if the current user isn't the author
-#     """
-#     conn = get_conn_db()
-#     cur = conn.cursor()
-#     cur.execute(
-#             "SELECT post.id, title, body, created, author_id, username"
-#             " FROM post  JOIN author ON post.author_id = author.id"
-#             " WHERE post.id = %s",
-#             (id,),
-#         )
-#     cur_post = cur.fetchone()
-#     post = tp_to_dict(cur_post, cur)
-#     cur.close()
-#     conn.commit()
-#     conn.close()
-#
-#     if post is None:
-#         abort(404, "Post id {0} doesn't exist.".format(id))
-#
-#     if check_author and post["author_id"] != g.user["id"]:
-#         abort(403)
-#
-#     return post
-#
-#
-# @bp.route("/create", methods=("GET", "POST"))
-# @login_required
-# def create():
-#     """Create a new post for the current user."""
-#     if request.method == "POST":
-#         title = request.form["title"]
-#         body = request.form["body"]
-#         error = None
-#
-#         if not title:
-#             error = "Title is required."
-#
-#         if error is not None:
-#             flash(error)
-#         else:
-#             conn = get_conn_db()
-#             cur = conn.cursor()
-#             cur.execute(
-#                 "INSERT INTO post (title, body, author_id)" " VALUES (%s, %s, %s)",
-#                 (title, body, g.user["id"]),
-#             )
-#             cur.close()
-#             conn.commit()
-#             conn.close()
-#
-#             return redirect(url_for("blog.index"))
-#
-#     return render_template("blog/create.html")
-#
-#
-# @bp.route("/<int:id>/update", methods=("GET", "POST"))
-# @login_required
-# def update(id):
-#     """Update a post if the current user is the author."""
-#     post = get_post(id)
-#
-#     if request.method == "POST":
-#         title = request.form["title"]
-#         body = request.form["body"]
-#         error = None
-#
-#         if not title:
-#             error = "Title is required."
-#
-#         if error is not None:
-#             flash(error)
-#         else:
-#             conn = get_conn_db()
-#             cur = conn.cursor()
-#             cur.execute(
-#                 "UPDATE post SET title = %s, body = %s WHERE id = %s", (title, body, id)
-#             )
-#             cur.close()
-#             conn.commit()
-#             conn.close()
-#
-#             return redirect(url_for("blog.index"))
-#
-#     return render_template("blog/update.html", post=post)
-#
-#
-# @bp.route("/<int:id>/delete", methods=("POST",))
-# @login_required
-# def delete(id):
-#     """Delete a post.
-#
-#     Ensures that the post exists and that the logged in user is the
-#     author of the post.
-#     """
-#     conn = get_conn_db()
-#     cur = conn.cursor()
-#     cur.execute(
-#         "DELETE FROM post WHERE id = %s", (id,)
-#     )
-#     cur.close()
-#     conn.commit()
-#     conn.close()
-#     return redirect(url_for("blog.index"))
